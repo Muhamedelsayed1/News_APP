0059fa3bfc7b90148fc3da30194ce108d976ae48
Muhamedelsayed1 <92642856+Muhamedelsayed1@users.noreply.github.com>
2022-05-15 20:47:50 +0200

    Room: article table and Dao is done

---
+++ app/src/main/java/com/example/newsapp/db/ArticleDao.kt
@@ -0,0 +1,18 @@
+package com.example.newsapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.newsapp.Article
+
+@Dao
+interface ArticleDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article:Article):Long   //this method check if the article already in the db it will update it and will return all available articles in db
+        @Query("SELECT * FROM articles")
+        fun getAllArticles():LiveData<List<Article>> // that mean if any change happen in article livedata will notify and make the change
+        @Delete
+        suspend fun deleteArticle(article:Article)
+
+
+
+    }
